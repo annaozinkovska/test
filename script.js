cb6df223f9f66aae655260293831f932fb9ec986
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2022-12-15 04:13:12 +0200

    2 home work lesson js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-window.onload = function(){
-    console.log('You can ignore single and double quotes like this :\\\'\\\'\\\'\\\"\\\" ')
-}
+ window.onload = function(){
+    let name= String(prompt('Enter you name'));
+alert('You name is :' + name );
+let surname = String(prompt('Enter you surname:'));
+alert('You full name is :' + name + " " + surname );
+let old= Number(prompt('How old are you ?' ));
+alert('You full name is :' + name + " " + surname + ", " + 'your age is:' +" "+ old );
+let Admin = confirm("Are you admin?");
+
+alert('You full name is :' + name + " " + surname + ", "
+ + 'your age is:' +" "+ old + ","+ 'admin status :' + Admin);
+
+ alert("Please enter your data")
+ }
+
+
