d44cdaee4930d71f36fa08c9bb569b88a7e4986c
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2022-12-15 22:15:26 +0200

    WHAT SERVICES +js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
- window.onload = function(){
-    alert(Boolean(true > null))
-//     let name= String(prompt('Enter you name'));
-// alert('You name is :' + name );
-// let surname = String(prompt('Enter you surname:'));
-// alert('You full name is :' + name + " " + surname );
-// let old= Number(prompt('How old are you ?' ));
-// alert('You full name is :' + name + " " + surname + ", " + 'your age is:' +" "+ old );
-// let Admin = confirm("Are you admin?");
 
-// alert('You full name is :' + name + " " + surname + ", "
-//  + 'your age is:' +" "+ old + ","+ 'admin status :' + Admin);
+    alert('Данний сайт буде гарно виглядати при розширенні екрана до 320пікселів або при від 998 -1090 пікселі');
+    
 
-//  alert("Please enter your data")
- }
+ 
 
 
