35f023fe5a8e29fc911c1a72b8c00fc65789e86e
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2022-12-23 04:40:29 +0200

    animation

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 
-    // alert('Данний сайт буде гарно виглядати при розширенні екрана до 320пікселів або при від 998 -1090 пікселі');
-    
 
- 
-
-
+//    let numa = Number(prompt('Enter first month salary', 0));
+//    let numb = Number(prompt('Enter second month salary', 0));
+//    let months = numa+numb;
+//    alert ("You salary for 2 monht is : " + months +"$" );
+//    let bonus = 1
+//     let plus = numa+numb+bonus
+//    alert ("You have extra bonus! Your total salary was: " + months +"$" + " It's increase for 1 $ and now it is:" + plus +"$");
+   
+// //    let  want = ((plus >= 2000 ) && (plus <= 2000 ));  
+// //    alert ( "Will you work next? " + want )  ;  
+// let  want = ((plus >= 2000 )  (plus <= 2000 ));  
+//     alert ( "Will you work next? " + want )  ; 
