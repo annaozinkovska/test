98a191b38b1c94be215ae6c1fc7160688fb78470
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2022-12-28 03:44:40 +0200

    js18 lesson

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
 
-//    let numa = Number(prompt('Enter first month salary', 0));
-//    let numb = Number(prompt('Enter second month salary', 0));
-//    let months = numa+numb;
-//    alert ("You salary for 2 monht is : " + months +"$" );
-//    let bonus = 1
-//     let plus = numa+numb+bonus
-//    alert ("You have extra bonus! Your total salary was: " + months +"$" + " It's increase for 1 $ and now it is:" + plus +"$");
-   
-// //    let  want = ((plus >= 2000 ) && (plus <= 2000 ));  
-// //    alert ( "Will you work next? " + want )  ;  
-// let  want = ((plus >= 2000 )  (plus <= 2000 ));  
-//     alert ( "Will you work next? " + want )  ; 
+   let numa = Number(prompt('Enter first month salary', 0));
+   let numb = Number(prompt('Enter second month salary', 0));
+   let months = numa+numb;
+   alert ("You salary for 2 monht is : " + months +"$" );
+   let bonus = 1
+    let plus = numa+numb+bonus
+   alert ("You have extra bonus! Your total salary was: " + months +"$" + " It's increase for 1 $ and now it is:" + plus +"$");
+   if(plus>=2000 ) alert("Im ready to work");
+   else alert("Im out");
+
+
+  
+   // alert ( "Will you work next? " + Boolean(plus >= 2000 ) )  ;  
+
+  
