678e1dd6707ce08c65b2536d07bc3b813653c6ac
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2023-01-07 03:09:24 +0200

    js 20lesson functiol

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
 
+// let a = prompt( "Enter first number");
+// let b = prompt( "Enter second number");
+// function maxNumber(a, b) {
+// if (+a > +b) {
+//     show = function () {
+//        alert(+a)
 
-   let numa = Number(prompt('Enter first month salary', 0));
-   let numb = Number(prompt('Enter second month salary', 0));
-   let months = numa+numb;
-   alert ("You salary for 2 monht is : " + months +"$" );
-   let bonus = 1
-    let plus = numa+numb+bonus
-   alert ("You have extra bonus! Your total salary was: " + months +"$" + " It's increase for 1 $ and now it is:" + plus +"$");
-   if(plus>=2000 ) alert("Im ready to work");
-   else alert("Im out");
+//     }
+// }
+//  if  (+a < +b) {
+//   show = function () {
+//      alert(+b)
+
+//   }
+// }
+// if (a === b) {
+//   show = function () {
+//      alert(`${a} = ${b}`)
+//   }
+
+//     }}
+// maxNumber(a, b);
+// show();
 
 
-  
-   // alert ( "Will you work next? " + Boolean(plus >= 2000 ) )  ;  
+// let number = prompt( "Enter  number");
+//  function  revers  (number) {
+//   if (number>0 ) {
+//     number = ( `${0-number}`);
+//     alert(number)}
+//  else (number<0 )
+//  { number =  (`${number*-1}`);
+//     alert(number) 
+//     }}
+//  revers(number);
+//  let number = prompt(`Введіть число яке множити на три `);
+//  let count = prompt(` Скільки разів множити на три `);
 
-  
+// function tree (number,count) {
+// count= number**3;
+//     alert(`Ваше число ${number} у третій степені ${count}`);
+// }
+// tree(number, count);
+
+
+
+
+
+
+let km = prompt('Ведіть кілометри?');
+let metrik = prompt('Ведіть в  яку одиницю виміру переводити километри в СМ чи М?');
+let m =1000;
+let cm =100000;
+
+function getMetric(metrik,km,m,cm) {
+    if (metrik =='m') {
+         KmToM =(km)=>km*m;
+         return metrik = KmToM(km)}
+    else if (metrik =='cm') { 
+         KmToCm =(km)=>km*cm; 
+        return metrik = KmToCm(km)}
+};
+
+alert(getMetric(metrik,km,m,cm));
+
+
