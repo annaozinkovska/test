0859dba1713721e28fe94f413ab7ad88e24251c3
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2023-01-27 03:18:16 +0200

    rekyrsia js 23 lesson

--- script.js
+++ script.js
@@ -1,61 +1,102 @@
-// let a = [`Mike`, `Nikola`, `Tom`];
-// console.log(a);
-// console.log(a[2]);
-// a [1] ='Alex';
-// console.log(a [1]);
-// a.length=1;
-// console.log(a);
-// console.log(a.length);
 
-// let a =[5, 3, -4, 25, 32, -16, 6];
-// let b = [ 21, -30, 9, 5, 12, -19, 5, 25];
-// function rut(a,b) {
-//   let i =0;
 
-// for (let i= 0; i<a.length;i++) {
-//   console.log (`${a[i]} - ${b[i]}  `);
-//   if  (a[i]<b[i])
-//   console.log(`${a[i]} < ${b[i]}`);
-//   if (a[i]==b[i]) 
-//  console.log(`${a[i]} = ${b[i]}`);
-//  if (a[i]>b[i]) 
-//  console.log(`${a[i]} > ${b[i]}`);
+// let arr =[1,2,3,4,5,6,7,8,9,10];
+// function sum(arr) {
+// if(arr === 55){
+//   return arr;
+// }  else {
+//   return  sum(arr+1);
+  
+// }
+// };
+// console.log(sum(arr[0]));
 
+
+
+// let arr =[1,2,3,4,5,6,7,8,9,10];
+// function sum(arr) {
+//    return (arr === 55)
+//   ? arr
+//  : arr=sum(++arr);
+  
+// };
+// console.log(sum(arr[0]));
+
+
+
+
+// console.log( deepCount( []                ));     // --> 0
+// console.log( deepCount( [1 ,2 ,3]         ));    // --> 3
+// console.log( deepCount( ["x", "z", ["y"]] ));   // --> 4
+// console.log( deepCount( [1, 2,[3, 4,[5]]] ));  // --> 7
+// console.log( deepCount( [[[[]]]]          )); // --> 3   
+
+
+
+
+
+
+
+
+
+
+
+// function deepCount(arr) {
+//     let result =arr.length ;
+//    for (const elm of arr) {
+//  if ( Array.isArray(elm)) { 
+//   result += deepCount(elm) ;
+
+// }   
+
+//  }
+
+//   return result  ;
 // };
 
-  
 
-// let phrase = 'I, am, learning,  JavaScript, right, now,';
-//   phrase = phrase.split(',');
-// console.log(phrase);
-
-
-
-// let a = [5, 3, 8, 5, 3, 2, 1, 2,];
-// let b = [];
-
-// a.forEach(function (car) {
-
-//     if (b.indexOf(car) === -1) {
-        
-//         b.push(car);   
+// function deepCount( arr )
+//   {
+//   let count = arr.length;
+//   for (const elm of arr) 
+//     {
+//     count += Array.isArray(elm) 
+//     ? deepCount(elm) 
+//     : 0;
 //     }
-// });
-//   console.log(b );
+//   return count;
+//   }
 
 
-// let user=[{id:1,age:17 },
-//   {id:2, age:18 },
-//   {id:3, age:19 },
-//   {id:4, age:21 },
-//   {id:5, age:17 },
-//   {id:6, age:20 },
-//   {id:7, age:25 },
 
-// ]; 
-// function SelectionAge(user) {
-  
 
-// console.log( user.filter(us => us.age > 18 && us.age <= 20));};
-
-//  SelectionAge(user)
+// let employees = {
+//   development: {
+//       backend: [{name: 'Mike', salary: 2000}, {name: 'Nikola', salary: 2500}],
+//       frontend: [{name: 'Artem', salary: 3000}, {name: 'Alex', salary: 2700}],
+//   },
+//   sales: {
+//       marketing: {
+//           internet_marketers: [{name: 'Nina', salary: 1000}, {name: 'Olena', salary: 1300}],
+//           promotion: [{name: 'Oleg', salary: 1400}, {name: 'Masha', salary: 1700}],
+//       },
+//       sellers:  [{name: 'Max', salary: 900}, {name: 'Donald', salary: 700}, {name: 'Joe', salary: 1100}]
+//   },
+//   designer: [{name: 'Kate', salary: 1800}]
+// }
+// // let sum = 0;
+// function takeSumSallary(){
+//   let sum = 0;
+//   return function sumSallary(obj){
+//       for(let key in obj){
+//           if (Array.isArray(obj[key])){
+//             obj[key].forEach(employee => sum += employee.salary);
+//           } else {
+//              sumSallary(obj[key]);
+//           }
+//       }
+//        console.log(sum)
+//       return sum;
+//   }
+// }
+// takeSumSallary()(employees);
