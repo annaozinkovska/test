a07a8097fd0fdcfaa49f823d7515baec2a4f2063
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2023-02-20 11:13:37 +0200

    OOP

--- app.js
+++ app.js
@@ -1,120 +1,156 @@
 
 
-
-// // Урок 26  Спадкування  
-
-// //Існує два об'єкта
-// // 1
-// //Зробіть так . Щоб об'єкт споркар успадкував властивості від об'єкту машина 
-// //Зробіть  об'єкт пасажир кар . який буде мати властивість дорс та успадкуе від каар 
-// //Зробіть  об'єкт іграшкова машина . який буде мати властивість тип та успадкуе властивості від спорт кар та каар 
-
-
-
-// let car ={
-//   type:"electric",
-//   wheels:4,
-
-// };
-
-// let sportCar ={
-//   doors:2
-
-// };
-// Object.setPrototypeOf(sportCar, car );
-// // console.log(car.type);
-// let passengerCar= {
-//   doors :4
-// };
-// Object.setPrototypeOf(passengerCar ,car);
-
-//     let toyCar= {
-//       type :"toy",
-//       // вивід всіх ключів в іграшковій машині для перевірки  
-// reVerification(){
-//   for ( key in this){
-//     console.log(key );
-// }
-// },
-//     };
-// Object.setPrototypeOf(toyCar, car);
-// Object.setPrototypeOf( toyCar, sportCar);
-// toyCar.reVerification();
-
-
-
-// // 2
-
-// // Існує такий код 
-// // Зараз про наразуванні заробітньої плати 
-// // одному працівнику .вона поповнюеться й у 
-// // іншого .Виправте код таким чином .
-// //  щоб заробітня плата нараховувалась окремо для кожного працівника.
-// let employees = {
-//   wallet: {},
-//   pay(munth, sum){
-//       this.wallet[munth] = sum;
+// 1. Створіть клас User, в якому зробіть захищену властивість name і публічні властивосі login та
+// age . Завдяки класу створіть 2 об'єкти  user1 user2
+// Виведіть у консоль властивості цих користувачів:
+// class User {
+//   constructor(name, login, age){
+//     this.name=name;
+//     this.login=login;
+//     this.age=age;
 //   }
 // }
+// let user1= new User ('Mike','MK_18' ,18 )
+// let user2= new User ('','NRG' ,22 )
+// console.log(user1.login);
+// console.log(user1.age);
+// console.log(user2.login);
+// console.log(user2.age);
 
-// let frontendDeveloper = {
-//   name: 'Mike',
-// }
-// Object.setPrototypeOf(frontendDeveloper,employees)
-// frontendDeveloper.wallet = {}
-
-// let backendDeveloper = {
-//   name: 'Bob'
-// }
-// Object.setPrototypeOf(backendDeveloper,employees);
-// backendDeveloper.pay('june',1500);
-// frontendDeveloper.pay('june',1700)
-
-// console.log(backendDeveloper.wallet.june);
-// console.log(frontendDeveloper.wallet.june);
+// // 2 Створіть  У класі User методи getName(isAdmin) ,
+// //  який повертає ім'я користувача або його нік,
+// //  якщо ім'я не вказано, або відмовляє у доступі, якщо аргумент isAdmim==false
 
 
+// class User {
+//   constructor(name, login, age) {
+//     this.name = name;
+//     this.login = login;
+//     this.age = age;
+//   }
+//   getName(isAdmin) {
 
 
-// // 3 У вас є такий код
-// // Використовуючи об'єкт юзер1
-// //  створіть нового користувача
-// //  юзер2 який буде мати ім'я боб та вік 25 
+//     if (this.name == "" || this.login == "") {
+//       console.log('Permission denied');
+//       return isAdmin == false;
+//     }
 
-//  function User(name, age) {
-//   this.name= name;
-//   this.age=age;
-
-
-// }
-
-//  let user_1 = new User ('mike', 18);
-//  user_1.constructor === User;
-//  let user_2= new user_1.constructor('bob', 25)
-//  console.log(user_2);
+//     else {
+//       console.log(`${this.name} ${this.login}`);
+//       return isAdmin == true;
+//     }
+//   }
 
 
-// 4 Існує Функція конструктор , яка створюе
-//  псевдо-масив (об'єкт , схожий на масив ) 
-function UserType(name) {
-  for (let i = 0; i < name.length; ++i) {
-    this[i] = name[i];
-    if (i + 1 == name.length) {
-      Object.defineProperty( this , 'length', {
-        enumerable: true,
-        writable: false,
-        configurable: true,
-        value: i + 1
+// //  3 Створіть  У класі User метод ChangeName  , 
+// // який змінює ім'я користувача. Змінна повинна бути
+// //  підтверджена введенням паролю`123` 
+// // . Якщо пароль збігається, то ім'я змінюється, а якщо ні,
+// //  то виводиться попередження Permission denied.
+// //  Після зміни імені програма повинна виводити таке повідомлення:
+// // Name Changed from Mike to Bill 
 
-      });
+
+//   ChangeName(pass, namechange ) {
+//     let roks = this.name;
+//       if (pass == `123`) {
+
+//         this.name = namechange;
+//          console.log(pass, this.name);
+//          return console.log(`Name Changed from ${roks} to ${this.name} `);
+
+
+//       } else {
+//         console.log(`Permission denied `);
+//       }
+//     }
+
+//   }
+// ;
+
+
+// // 4 Зробіть клас  Admin, розширюючи його класом User.
+// //  Клас Admin повинен приймати всі властивості name login age 
+// //  , від класу User, а також мати власну приватну властивість isAdmin==true
+// //  . Зробіть у цьому класі метод getUserName(User) , 
+// //  який буде приймати користувача та виводити його ім'я. Приклад:
+
+// class Admin extends User{
+//    #isAdmin;
+//   constructor (name,login , age, isAdmin){
+//     super(name, login , age);
+//     this.#isAdmin = true
+//   }
+//   getUserName() {
+//  return this.name; 
+//   }
+// };  
+
+// let user1 = new User('Mike', 'MK_18', 18);
+// let user2 = new User('', 'NRG',``, 22);
+// let user3 = new Admin('NRG', 'DVDF', ``, 22);
+// user1.getName(true);
+// user2.getName();
+// user2.Admin();
+// user2.ChangeName(123,'MK_18' )
+// console.log( user3.getUserName);
+
+//  console.log(user2.getUserName());
+// console.log(); user1.getUserName();
+
+
+
+// 5 Створіть  новий класс User, 
+// , який буде приймати публічну властивість name та приватну властивість phone. 
+// Властивість phone завжди буде мати такий формат '067-888-88-99'.
+//  Створіть метод getPhone(isAdmin).
+//  Якщо аргумент isAdmin повертає true,
+//  то номер виводиться повністю, а якщо false, 
+// то виводиться частково прихований номер. Приклад:
+
+class User {
+    #passwort
+    constructor(name, passwort) {
+      this.name = name;
+      this.#passwort = passwort;
+  
     }
+  
+    getPhon( ) {
+  
+      let arr = this.#passwort.split('');
+      
+    //   console.log(arr);
+      if (arr.length === 10) {
+        let ro = "-";
+        arr.splice(3, 0, ro);
+        arr.splice(7, 0, ro);
+        arr.splice(10, 0, ro);
+        // console.log(arr.join(""));
+      }
+      else {
+        console.log('Ведите правильный номер ');
+      }
+      for (let key in arr) {
+  
+        if (key >= 4 && key <= 9)
+          arr[key] = ("*");
+        let to = arr[key];
+      }
+  
+      console.log(arr.join(""));
+  
+    }
+    getPhone(isAdmin) {
+          return isAdmin
+            ? this.#passwort
+            : this.getPhon()
+        }
+  };
 
-  }
-}
-
-// // // Зробіть так щоб ця Функція встановлювала
-// // //  об'єктам в якості прототипу позичений метод ....
-// // // Ви повинні отримати такий результат
-let admins = new UserType([ 'Mike', 'Bob', 'Nikola']);
-  admins.join = Array.prototype.join;
-console.log(admins.join(`; `) )
+  const users = new User("anna", "0932049217");
+// users.getPhon();
+let user5 = new User("anna","0932049217")  
+ console.log( users.getPhone(true));
+ console.log( users.getPhone(false));
--- bus/app.js
+++ bus/app.js
@@ -1,20 +1,25 @@
 
 
+// 1. Створіть клас User, в якому зробіть захищену властивість name і публічні властивосі login та
+// age . Завдяки класу створіть 2 об'єкти  user1 user2
+// Виведіть у консоль властивості цих користувачів:
+// class User {
+//   constructor(name, login, age){
+//     this.name=name;
+//     this.login=login;
+//     this.age=age;
+//   }
+// }
+// let user1= new User ('Mike','MK_18' ,18 )
+// let user2= new User ('','NRG' ,22 )
+// console.log(user1.login);
+// console.log(user1.age);
+// console.log(user2.login);
+// console.log(user2.age);
 
-
-// // class User {
-// //   constructor(name, login, age){
-// //     this.name=name;
-// //     this.login=login;
-// //     this.age=age;
-// //   }
-// // }
-// // let user1= new User ('Mike','MK_18' ,18 )
-// // let user2= new User ('','NRG' ,22 )
-// // console.log(user1.login);
-// // console.log(user1.age);
-// // console.log(user2.login);
-// // console.log(user2.age);
+// // 2 Створіть  У класі User методи getName(isAdmin) ,
+// //  який повертає ім'я користувача або його нік,
+// //  якщо ім'я не вказано, або відмовляє у доступі, якщо аргумент isAdmim==false
 
 
 // class User {
@@ -36,11 +41,20 @@
 //       return isAdmin == true;
 //     }
 //   }
+
+
+// //  3 Створіть  У класі User метод ChangeName  , 
+// // який змінює ім'я користувача. Змінна повинна бути
+// //  підтверджена введенням паролю`123` 
+// // . Якщо пароль збігається, то ім'я змінюється, а якщо ні,
+// //  то виводиться попередження Permission denied.
+// //  Після зміни імені програма повинна виводити таке повідомлення:
+// // Name Changed from Mike to Bill 
+
+
 //   ChangeName(pass, namechange ) {
-//     // let pass = prompt("ведите пароль ");
 //     let roks = this.name;
 //       if (pass == `123`) {
-//         // let namechange = prompt("ведыть нове ымья користувача");
 
 //         this.name = namechange;
 //          console.log(pass, this.name);
@@ -55,6 +69,13 @@
 //   }
 // ;
 
+
+// // 4 Зробіть клас  Admin, розширюючи його класом User.
+// //  Клас Admin повинен приймати всі властивості name login age 
+// //  , від класу User, а також мати власну приватну властивість isAdmin==true
+// //  . Зробіть у цьому класі метод getUserName(User) , 
+// //  який буде приймати користувача та виводити його ім'я. Приклад:
+
 // class Admin extends User{
 //    #isAdmin;
 //   constructor (name,login , age, isAdmin){
@@ -62,7 +83,6 @@
 //     this.#isAdmin = true
 //   }
 //   getUserName() {
-//   //  let name= this.name;
 //  return this.name; 
 //   }
 // };  
@@ -72,54 +92,61 @@
 // let user3 = new Admin('NRG', 'DVDF', ``, 22);
 // user1.getName(true);
 // user2.getName();
-//user2.Admin();
-//user2.ChangeName(123,'MK_18' )
+// user2.Admin();
+// user2.ChangeName(123,'MK_18' )
 // console.log( user3.getUserName);
 
 //  console.log(user2.getUserName());
-// console.log(); user1.getUserName());
+// console.log(); user1.getUserName();
 
 
 
+// 5 Створіть  новий класс User, 
+// , який буде приймати публічну властивість name та приватну властивість phone. 
+// Властивість phone завжди буде мати такий формат '067-888-88-99'.
+//  Створіть метод getPhone(isAdmin).
+//  Якщо аргумент isAdmin повертає true,
+//  то номер виводиться повністю, а якщо false, 
+// то виводиться частково прихований номер. Приклад:
 
-// class User {
-//   #passwort
-//   constructor(name, passwort) {
-//     this.name = name;
-//     this.#passwort = passwort;
+class User {
+  #passwort
+  constructor(name, passwort) {
+    this.name = name;
+    this.#passwort = passwort;
 
-//   }
-//   getPhone(isAdmin) {
-//     return isAdmin
-//       ? this.#passwort.join
-//       : this.getPhon()
-//   }
-//   getPhon(arr) {
+  }
+  getPhone(isAdmin) {
+    return isAdmin
+      ? this.#passwort.join
+      : this.getPhon()
+  }
+  getPhon( ) {
 
-//     let arr = this.#passwort.split('');
-//     // arr.split('');
-//     // console.log(arr);
-//     if (arr.length === 10) {
-//       let ro = "-";
-//       arr.splice(3, 0, ro);
-//       arr.splice(7, 0, ro);
-//       arr.splice(10, 0, ro);
-//       // console.log(arr.join(""));
-//     }
-//     else {
-//       console.log('Ведите правильный номер ');
-//     }
-//     for (let key in arr) {
+    let arr = this.#passwort.split('');
+    arr.split('');
+    console.log(arr);
+    if (arr.length === 10) {
+      let ro = "-";
+      arr.splice(3, 0, ro);
+      arr.splice(7, 0, ro);
+      arr.splice(10, 0, ro);
+      console.log(arr.join(""));
+    }
+    else {
+      console.log('Ведите правильный номер ');
+    }
+    for (let key in arr) {
 
-//       if (key >= 4 && key <= 9)
-//         arr[key] = ("*");
-//       let to = arr[key];
-//     }
+      if (key >= 4 && key <= 9)
+        arr[key] = ("*");
+      let to = arr[key];
+    }
 
-//     console.log(arr.join(""));
+    console.log(arr.join(""));
 
-//   }
-// };
+  }
+};
 
 
 // const users = new User("anna", "0932049217");
