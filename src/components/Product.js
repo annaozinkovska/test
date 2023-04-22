e48a993ed9e795a38540acf0eb7375257b33cd92
Annaozinkovska <118577916+annaozinkovska@users.noreply.github.com>
2023-04-22 13:31:04 +0300

    44 css imj

---
+++ src/components/Product.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Product(props){
+    const remove = () => {
+        props.onRemove(props.id)
+    }
+
+    return (
+        <div className="product">
+            <div className="img">
+                <img src={props.img} />
+            </div>
+
+            <div className="body">
+                <h1 className="title">{props.name}</h1>
+                <div className="price">{props.price}</div>
+            </div>
+            <button onClick={remove} type="button">Remove</button>
+        </div>
+    )
+}
+
+export default Product;
